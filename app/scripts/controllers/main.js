a5f25cdfeec77b4912d5e1bec46232cc273a0c5c
David Thibault <thibault.david@gmail.com>
2013-10-15 17:28:56 -0400

    Minor code cleanup

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,13 +2,4 @@
 
 angular.module('homepageApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.calendar = {
-      config: {
-        
-      },
-      events: [
-        "https://www.google.com/calendar/feeds/thibault.david%40gmail.com/public/basic"
-        //"https://www.google.com/calendar/feeds/thibault.david%40gmail.com/private-793258c7e9f2f65a5bf33a76ead11cbb/basic"
-      ]
-    }
   });
