cb89d8d00ccf701d2abc4491ab97c6cd2e7f4df5
David Thibault <thibault.david@gmail.com>
2013-10-15 18:14:18 -0400

    Auto-refresh feeds every 5 minutes (+ refresh now button)

--- app/scripts/directives/feed.js
+++ app/scripts/directives/feed.js
@@ -1,33 +1,44 @@
 'use strict';
 
 angular.module('homepageApp')
-  .directive('feed', function (FeedService) {
+  .directive('feed', function (FeedService, $timeout) {
     return {
       templateUrl: '/views/directives/feed.html',
       restrict: 'E',
       scope: true,
       link: function postLink(scope, element, attrs) {
         var count = parseInt(attrs.count || "10");
+        var timeout;
         
-        FeedService.parseFeed(attrs.url).then(function(response) {
-          var feed = response.data.responseData.feed;
+        scope.refresh = function() {
+          if (timeout) {
+            $timeout.cancel(timeout);
+          }
 
-          scope.title = feed.title;
-          scope.link = feed.link;
-          scope.items = _.chain(feed.entries)
-            .take(count)
-            .map(function (entry) {
-              return {
-                title: entry.title,
-                link: entry.link,
-                content: (attrs.display == "snippet" ? entry.contentSnippet : entry.content),
-                tags: entry.categories,
-                date: entry.publishedDate ? new Date(entry.publishedDate) : null
-              };
-            })
-            .value();
-           
-        });
+          FeedService.parseFeed(attrs.url).then(function(response) {
+            var feed = response.data.responseData.feed;
+
+            scope.lastRefresh = new Date();
+            scope.title = feed.title;
+            scope.link = feed.link;
+            scope.items = _.chain(feed.entries)
+              .take(count)
+              .map(function (entry) {
+                return {
+                  title: entry.title,
+                  link: entry.link,
+                  content: (attrs.display == "snippet" ? entry.contentSnippet : entry.content),
+                  tags: entry.categories,
+                  date: entry.publishedDate ? new Date(entry.publishedDate) : null
+                };
+              })
+              .value();
+             
+            timeout = $timeout(scope.refresh, 5 * 60 * 1000); // refresh every 5 minutes
+          });
+        }
+
+        scope.refresh();
       }
     };
   });
