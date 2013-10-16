aa48439038893cce151c0e5a98f7b798784067b0
David Thibault <thibault.david@gmail.com>
2013-10-15 23:32:59 -0400

    Create a directive to encapsulate the google calendar iframe

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,4 +2,9 @@
 
 angular.module('homepageApp')
   .controller('MainCtrl', function ($scope) {
+    $scope.calendars = [
+      {src:'thibault.david@gmail.com', color: '#2952A3'},
+      {src:'2g5hhq1d0nk373earfrnlb37k8@group.calendar.google.com', color: '#B1365F'},
+      {src:'david.thibault@sigmund.ca', color: '#8C500B'}
+    ];
   });
---
+++ app/scripts/directives/gcal.js
@@ -0,0 +1,23 @@
+'use strict';
+
+angular.module('homepageApp')
+  .directive('gcal', function () {
+    return {
+      template: '<iframe src="https://www.google.com/calendar/embed?showTitle=0&amp;showPrint=0&amp;showTz=0&amp;showCalendars=0&amp;bgcolor=%23fafafa&amp;wkst=1&amp;{{calendarParameters}}ctz=America%2FNew_York" style=" border-width:0 " width="{{width}}" height="{{height}}" frameborder="0" scrolling="no"></iframe>',
+      restrict: 'E',
+      scope: {
+        calendars: '=',
+        width: '@', 
+        height: '@'
+      },
+      link: function postLink(scope, element, attrs) {
+        var params = '';
+        for (var i = 0; i < scope.calendars.length; i++) {
+          var c = scope.calendars[i];
+          params += 'src=' + encodeURIComponent(c.src) + '&color=' + encodeURIComponent(c.color) + '&';
+        }
+
+        scope.calendarParameters = params;
+      }
+    };
+  });
